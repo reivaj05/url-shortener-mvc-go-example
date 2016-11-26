20fc1a04ca3b3555841d0eef6da131b5c4496ebc
Javier Adriel Sanchez <reivaj0705@gmail.com>
2016-11-25 18:07:07 -0600

    Add post item controller

--- shortener/controllers.go
+++ shortener/controllers.go
@@ -2,6 +2,7 @@ package shortener
 
 import (
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/reivaj05/GoDB"
@@ -40,7 +41,7 @@ func getItemHandler(rw http.ResponseWriter, req *http.Request) {
 	id, _ := strconv.Atoi(params["id"])
 	if err := dbClient.Get(&urlShortInstance, id); err != nil {
 		GoServer.SendResponseWithStatus(
-			rw, `{"error": "Resource not found"}`, http.StatusNotFound)
+			rw, GoServer.ResourceNotFound, http.StatusNotFound)
 		return
 	}
 	GoServer.SendResponseWithStatus(
@@ -48,8 +49,62 @@ func getItemHandler(rw http.ResponseWriter, req *http.Request) {
 }
 
 func postItemhandler(rw http.ResponseWriter, req *http.Request) {
+	data, err := getJSONData(req)
+	if err != nil {
+		sendBadRequestResponse(rw)
+		return
+	}
+	if !isDataValid(data) {
+		sendBadRequestResponse(rw)
+		return
+	}
+	if err := saveDataIntoDB(data); err != nil {
+		GoServer.SendResponseWithStatus(rw, "", http.StatusInternalServerError)
+		return
+	}
 	GoServer.SendResponseWithStatus(
-		rw, `{"msg": "implement post"}`, http.StatusOK)
+		rw, GoServer.ResourceCreated, http.StatusCreated)
+}
+
+func getJSONData(req *http.Request) (data *GoJSON.JSONWrapper, err error) {
+	body, _ := GoServer.ReadBodyRequest(req)
+	data, err = GoJSON.New(body)
+	return
+}
+
+func isDataValid(data *GoJSON.JSONWrapper) bool {
+	if data.HasPath("longURL") {
+		if longURL, ok := data.GetStringFromPath("longURL"); ok {
+			return isURLValid(longURL)
+		}
+	}
+	return false
+}
+
+func isURLValid(longURL string) bool {
+	_, err := url.ParseRequestURI(longURL)
+	return err == nil
+}
+
+func sendBadRequestResponse(rw http.ResponseWriter) {
+	GoServer.SendResponseWithStatus(
+		rw, GoServer.BadRequest, http.StatusBadRequest)
+}
+
+func saveDataIntoDB(data *GoJSON.JSONWrapper) error {
+	longURL, _ := data.GetStringFromPath("longURL")
+	shortURL := createShortURL(longURL)
+	dbClient := GoDB.GetDBClient()
+	return dbClient.Create(&URLShortModel{
+		LongURL:  longURL,
+		ShortURL: shortURL,
+	})
+}
+
+func createShortURL(longURL string) string {
+	// TODO: create short_url
+	// TODO: check if longURL exists and send appropiate response
+	return "short_url_of: " + longURL
 }
 
 func putItemHandler(rw http.ResponseWriter, req *http.Request) {
--- shortener/controllers_test.go
+++ shortener/controllers_test.go
@@ -1,6 +1,7 @@
 package shortener
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -41,16 +42,17 @@ func (suite *ControllersTestSuite) TearDownTest() {
 
 func createTestRouter() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/shortener/", getListHandler)
-	router.HandleFunc("/shortener/{id:[0-9]+}", getItemHandler)
+	router.Methods("GET").Path("/shortener/").HandlerFunc(getListHandler)
+	router.Methods("GET").Path("/shortener/{id:[0-9]+}").HandlerFunc(getItemHandler)
+	router.Methods("POST").Path("/shortener/").HandlerFunc(postItemhandler)
 	return router
 }
 
 func (suite *ControllersTestSuite) TestGetListHandler() {
 
-	suite.dbClient.Create(&URLShortModel{})
-	suite.dbClient.Create(&URLShortModel{})
-	response, status := suite.makeRequest("GET", "http://localhost/shortener/")
+	suite.dbClient.Create(&URLShortModel{LongURL: "long1", ShortURL: "short1"})
+	suite.dbClient.Create(&URLShortModel{LongURL: "long2", ShortURL: "short2"})
+	response, status := suite.makeRequest("GET", "http://localhost/shortener/", nil)
 	suite.assert.Equal(http.StatusOK, status)
 	jsonResponse, _ := GoJSON.New(response)
 	suite.assert.True(jsonResponse.HasPath("data"))
@@ -60,18 +62,55 @@ func (suite *ControllersTestSuite) TestGetListHandler() {
 
 func (suite *ControllersTestSuite) TestGetItemHandler() {
 	suite.dbClient.Create(&URLShortModel{})
-	_, status := suite.makeRequest("GET", "http://localhost/shortener/1")
+	_, status := suite.makeRequest("GET", "http://localhost/shortener/1", nil)
 	suite.assert.Equal(http.StatusOK, status)
 }
 
 func (suite *ControllersTestSuite) TestGetItemHandlerNotFound() {
-	_, status := suite.makeRequest("GET", "http://localhost/shortener/1")
+	_, status := suite.makeRequest("GET", "http://localhost/shortener/1", nil)
 	suite.assert.Equal(http.StatusNotFound, status)
 }
 
-func (suite *ControllersTestSuite) makeRequest(method, url string) (string, int) {
+func (suite *ControllersTestSuite) TestPostItemHandler() {
+	const body = `{"longURL": "http://www.example.com"}`
+	_, status := suite.makeRequest("POST", "http://localhost/shortener/", []byte(body))
+	suite.assert.Equal(http.StatusCreated, status)
+}
+
+func (suite *ControllersTestSuite) TestPostItemHandlerWithWrongJSON() {
+	_, status := suite.makeRequest("POST", "http://localhost/shortener/", []byte(""))
+	suite.assert.Equal(http.StatusBadRequest, status)
+}
+
+func (suite *ControllersTestSuite) TestPostItemHandlerWithoutCorrectFields() {
+	_, status := suite.makeRequest("POST", "http://localhost/shortener/", []byte("{}"))
+	suite.assert.Equal(http.StatusBadRequest, status)
+}
+
+func (suite *ControllersTestSuite) TestPostItemHandlerWithInvalidData() {
+	const body = `{"longURL": "not a url"}`
+	_, status := suite.makeRequest("POST", "http://localhost/shortener/", []byte(body))
+	suite.assert.Equal(http.StatusBadRequest, status)
+}
+
+func (suite *ControllersTestSuite) TestPostItemHandlerWithRepeatedData() {
+	suite.dbClient.Create(&URLShortModel{LongURL: "http://www.example.com", ShortURL: ""})
+	const body = `{"longURL": "http://www.example.com"}`
+	_, status := suite.makeRequest("POST", "http://localhost/shortener/", []byte(body))
+	suite.assert.Equal(http.StatusInternalServerError, status)
+}
+
+func (suite *ControllersTestSuite) makeRequest(
+	method, url string, body []byte) (string, int) {
+
 	rw := httptest.NewRecorder()
-	request, _ := http.NewRequest(method, url, nil)
+	var request *http.Request
+	if body != nil {
+		request, _ = http.NewRequest(method, url, bytes.NewBuffer(body))
+	} else {
+		request, _ = http.NewRequest(method, url, nil)
+	}
+
 	suite.router.ServeHTTP(rw, request)
 	res, _ := ioutil.ReadAll(rw.Body)
 	return string(res), rw.Code
